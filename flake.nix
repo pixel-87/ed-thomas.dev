0ac84dd5e7215642227b87f067c51f13da964cd4
pixel <edwardoliverthomas@gmail.com>
2025-08-13 16:30:27 +0100

    setting up theme

--- flake.nix
+++ flake.nix
@@ -7,7 +7,13 @@
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
-      buildInputs = [ nixpkgs.legacyPackages.x86_64-linux.hugo ];
+      buildInputs = [ 
+        nixpkgs.legacyPackages.x86_64-linux.hugo
+        nixpkgs.legacyPackages.x86_64-linux.git
+        nixpkgs.legacyPackages.x86_64-linux.nodejs
+        nixpkgs.legacyPackages.x86_64-linux.pandoc
+      nixpkgs.legacyPackages.x86_64-linux.go
+      ];
     };
   };
 }
