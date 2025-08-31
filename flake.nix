6b6dc4add0e53b0b182b228847a2d4a28553dd50
pixel <edwardoliverthomas@gmail.com>
2025-08-31 19:55:17 +0100

    feture:astro init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "My dev website, using astro";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    lib = pkgs.lib;
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        nodejs_24
+        alejandra
+      ];
+      shellHook = ''
+        echo "Entered Dev Shell"
+      '';
+    };
+  };
+}
