a0a099a9dbb5ed7579a810c31b495bb1798ad404
pixel <edwardoliverthomas@gmail.com>
2025-09-03 21:58:03 +0100

    not using pages anymore

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,11 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // When hosting on a custom domain, base should be '/'
-  // If you host as a project site (username.github.io/repo), set base to '/repo/'
   base: '/',
 
-  outDir: '../docs',
   site: 'https://ed-thomas.dev',
   integrations: [tailwind(), sitemap()],
   markdown: {
