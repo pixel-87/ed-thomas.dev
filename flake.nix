7c06c45c404580a153d3d7638013327a359a95f6
pixel <edwardoliverthomas@gmail.com>
2025-10-02 20:47:46 +0100

    refactor: remove unused Caddyfile generation and clean up flake outputs

--- flake.nix
+++ flake.nix
@@ -6,62 +6,23 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-parts,
-    }:
-    let
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-          system: function nixpkgs.legacyPackages.${system}
-        );
-    in
-    {
-      packages = forAllSystems (pkgs: rec {
-        example = pkgs.callPackage ./default.nix { };
-        default = self.packages.${pkgs.stdenv.hostPlatform.system}.example;
-
-        site = pkgs.dockerTools.buildImage {
-          name = "ed-thomas.dev";
-          tag = "latest";
-
-          fromImage = pkgs.dockerTools.pullImage {
-            imageName = "nginx";
-            imageDigest = "sha256:28402db69fec7c17e179ea87882667f1e054391138f77ffaf0c3eb388efc3ffb";
-            sha256 = "0cnq8cqmkqbp9gr4vp2zzsm5mjx1md2xj5q0xh5s3bjz8dkmm0fg";
-            finalImageName = "nginx";
-            finalImageTag = "alpine";
-          };
-
-          runAsRoot = ''
-            # Copy static site files to nginx default location
-            mkdir -p /usr/share/nginx/html
-            cp -a ${example}/. /usr/share/nginx/html/
-
-            # Ensure nginx user owns the files
-            chown -R nginx:nginx /usr/share/nginx/html
-          '';
-
-          config = {
-            ExposedPorts = {
-              "80/tcp" = { };
-            };
-            Cmd = [
-              "nginx"
-              "-g"
-              "daemon off;"
-            ];
-          };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+      
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages = {
+          site = pkgs.callPackage ./default.nix { };
+          default = config.packages.site;
         };
-      });
 
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./shell.nix { };
-      });
+        devShells.default = pkgs.callPackage ./shell.nix { };
+      };
 
-      overlays.default = final: _: { example = final.callPackage ./default.nix { }; };
+      flake = {
+        overlays.default = final: _: { 
+          ed-thomas-dev = final.callPackage ./default.nix { }; 
+        };
+      };
     };
 }
