c6cc1ddae5e08dfe0c25bf81b432f82bebd781cc
pixel <edwardoliverthomas@gmail.com>
2025-10-12 20:55:29 +0100

    typo in hash

--- nix/default.nix
+++ nix/default.nix
@@ -23,7 +23,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
-    hash = " sha256-LcSM5i1OmGIOhMjnI4nm1HbuRKJwgp7pGoPPIeWV0eM=";
+    hash = "sha256-LcSM5i1OmGIOhMjnI4nm1HbuRKJwgp7pGoPPIeWV0eM=";
   };
 
   env.ASTRO_TELEMETRY_DISABLED = 1;
