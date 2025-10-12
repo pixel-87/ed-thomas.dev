07aab60b64f89d31c7b1124a4fe5f312aa0b82d8
pixel <edwardoliverthomas@gmail.com>
2025-10-12 17:42:17 +0100

    feature: blog posts now use mdx, added images to keyboard post

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
 
   site: 'https://ed-thomas.dev',
-  integrations: [tailwind(), sitemap()],
+  integrations: [mdx(), tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-light',
