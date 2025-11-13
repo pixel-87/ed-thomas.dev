298e7e41448784d50c4e0213a142b2874c849a3a
pixel <edwardoliverthomas@gmail.com>
2025-11-13 15:41:11 +0000

    featured notes now works

--- site/src/content/config.ts
+++ site/src/content/config.ts
@@ -7,6 +7,7 @@ const blog = defineCollection({
     description: z.string(),
     pubDate: z.date(),
     tags: z.array(z.string()).optional(),
+    featured: z.boolean().default(false),
   }),
 });
 
